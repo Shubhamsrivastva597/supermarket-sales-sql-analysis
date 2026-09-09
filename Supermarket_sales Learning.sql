0e8d54567ce39349df4942e49b4003afe0f0daef
Shubhamsrivastva597 <srivastva597@gmail.com>
2026-09-10 01:12:34 +0530

    Add files via upload

--- Supermarket_sales Learning.sql
+++ Supermarket_sales Learning.sql
@@ -39,6 +39,79 @@ from supermarket_sales
 group by `Satisfaction level`
 ORDER BY `Total orders` DESC;
 
+Desc Supermarket_sales;
+
+Select  Count(`Invoice Id`) as `Total Order`, round(Avg(`Rating`),2) as `Avg Rating` from supermarket_Sales;
+
+Select `Invoice Id`,`Customer Type`, `Payment` from supermarket_Sales
+WHERE `Customer Type` = 'Member' AND `Payment` = 'Ewallet';
+
+select city, round(Sum(`total`),2) from supermarket_sales
+group by City;
+
+Select `Product line`, sum(Quantity) As `Total Qty Sold`from 
+supermarket_sales
+group by `product line`
+having `Total Qty Sold` > 500;
+
+Select `invoice Id`, Total from supermarket_sales
+WHERE `Total` > (Select Avg(`Total`) from supermarket_sales);
+
+SELECT `Date` FROM supermarket_sales LIMIT 5;
+
+Select monthname(STR_TO_DATE(`Date`, '%m/%d/%Y')) AS Month,
+Round(Sum(Total),2) As `Total Revenue`
+from Supermarket_Sales
+group by`Month`
+Order by `total Revenue` Desc;
+
+
+Select  Branch,`Product Line`, round(Sum(Total),2) AS `tOTAL REVENUE`, 
+DENSE_RANK () OVER(Partition By `Branch` Order by Sum(Total) Desc) AS `REVENUE rANK`
+from Supermarket_Sales
+Group By Branch, `Product Line`;
+
+
+
+Select `Invoice ID`, `Product Line`, Quantity
+From supermarket_sales
+where Quantity > (Select avg(Quantity) from supermarket_sales);
+
+Select round(Sum(Total),2) As Total_Revenue 
+from Supermarket_sales
+Where Total > (Select avg(Total) from supermarket_sales);
+
+
+Select DAYNAME(str_to_date(DATE, '%m/%d/%Y')) AS `Day Name`, round(Sum(Total),2) as `Total Revenue`
+from Supermarket_Sales
+Group By `Day Name`
+Order By `Total Revenue` desc;
+
+Select Branch, round(Sum(Total),2) as `Total Revenue`, 
+dense_rank()  Over(Order By Sum(Total)  DESC) as Revenue_Rank
+from Supermarket_Sales
+Group By Branch;
+
+Select `Invoice Id`, `Product Line`, Total,
+dense_rank() Over(Partition By `Product Line` order by Total DESC ) As `Product Line Ranking`
+from supermarket_Sales;
+
+Select City, `Product line`, round(Sum(Total),2) as `Total Revenue`,
+dense_rank () Over(partition by City Order by Sum(Total) DESC) AS `Rank in City`
+from supermarket_sales
+Group By City, `Product line`;
+
+With Ranksales AS (
+Select City, `Product line`, round(Sum(Total),2) as `Total Revenue`,
+dense_rank () Over(partition by City Order by Sum(Total) DESC) AS `Rank in City`
+from supermarket_sales 
+Group By City, `Product line`)
+Select City, `Product Line`, `Total Revenue`, `Rank in city` from Ranksales
+Where `Rank in city` = 1;
+
+
+
+
 
 
 
